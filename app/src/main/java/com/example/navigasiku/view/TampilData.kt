17eb49a82dbcec3594d41966ccc31947ad176981
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-11-07 09:45:24 +0700

    Menambahkan function

---
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -0,0 +1,13 @@
+package com.example.navigasiku.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick:() -> Unit
+){
+
+}
